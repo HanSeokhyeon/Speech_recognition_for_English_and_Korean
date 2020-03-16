4617b83a49795d5573c18fd90a8456037768fa57
hanseokhyeon <sah0322@naver.com>
2020-03-16 19:14:11 +0900

    del find

--- util/timit_preprocess.sh
+++ util/timit_preprocess.sh
@@ -3,6 +3,6 @@ if [ "$#" -ne 1 ]; then
 fi
 echo 'Transfering raw TIMIT wave file format from NIST to RIFF.'
 echo ' '
-find $1 -name '*.WAV' #| parallel -P20 sox {} '{.}.WAV'
+#find $1 -name '*.WAV' #| parallel -P20 sox {} '{.}.WAV'
 # MFCC
 python3 timit_preprocess.py $1 timit_mfcc_39
