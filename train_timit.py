d0a3d991203f638d6ea422e8e6c0aa2c65218e79
hanseokhyeon <sah0322@naver.com>
2020-03-18 11:51:54 +0900

    azizcode92 ver

--- train_timit.py
+++ train_timit.py
@@ -8,36 +8,38 @@ import torch
 import sys
 from tensorboardX import SummaryWriter
 import argparse
-from logger import *
 
 # Load config file for experiment
 parser = argparse.ArgumentParser(description='Training script for LAS on TIMIT .')
-parser.add_argument('config_path', metavar='config_path', type=str, help='Path to config file for training.')
+
+parser.add_argument('config_path', metavar='config_path', type=str,
+                    help='Path to config file for training.')
+
 paras = parser.parse_args()
+
 config_path = paras.config_path
-conf = yaml.load(open(config_path,'r'))
-if not torch.cuda.is_available():
-    conf['model_parameter']['use_gpu'] = False
+
+conf = yaml.load(open(config_path, 'r'))
 
 # Parameters loading
 torch.manual_seed(conf['training_parameter']['seed'])
-total_epochs = conf['training_parameter']['total_epochs']
+total_steps = conf['training_parameter']['total_steps']
 use_pretrained = conf['training_parameter']['use_pretrained']
 verbose_step = conf['training_parameter']['verbose_step']
-valid_step  = conf['training_parameter']['valid_step']
+valid_step = conf['training_parameter']['valid_step']
 tf_rate_upperbound = conf['training_parameter']['tf_rate_upperbound']
 tf_rate_lowerbound = conf['training_parameter']['tf_rate_lowerbound']
 
 # Load preprocessed TIMIT Dataset ( using testing set directly here, replace them with validation set your self)
 # X : Padding to shape [num of sample, max_timestep, feature_dim]
 # Y : Squeeze repeated label and apply one-hot encoding (preserve 0 for <sos> and 1 for <eos>)
-X_train, y_train, X_valid, y_valid, X_test, y_test = load_dataset(**conf['meta_variable'])
+X_train, y_train, X_val, y_val, X_test, y_test = load_dataset(**conf['meta_variable'])
 train_set = create_dataloader(X_train, y_train, **conf['model_parameter'], **conf['training_parameter'], shuffle=True)
-valid_set = create_dataloader(X_valid, y_valid, **conf['model_parameter'], **conf['training_parameter'], shuffle=False)
-test_set = create_dataloader(X_test, y_test, **conf['model_parameter'], **conf['training_parameter'], shuffle=False)
+valid_set = create_dataloader(X_val, y_val, **conf['model_parameter'], **conf['training_parameter'], shuffle=False)
+# test_set = create_dataloader(X_test, y_test, **conf['model_parameter'], **conf['training_parameter'], shuffle=False)
 
 # Construct LAS Model or load pretrained LAS model
-log_writer = SummaryWriter(conf['meta_variable']['training_log_dir']+conf['meta_variable']['experiment_name'])
+log_writer = SummaryWriter(conf['meta_variable']['training_log_dir'] + conf['meta_variable']['experiment_name'])
 
 if not use_pretrained:
     listener = Listener(**conf['model_parameter'])
@@ -45,57 +47,44 @@ if not use_pretrained:
 else:
     listener = torch.load(conf['training_parameter']['pretrained_listener_path'])
     speller = torch.load(conf['training_parameter']['pretrained_speller_path'])
-optimizer = torch.optim.Adam([{'params':listener.parameters()}, {'params':speller.parameters()}],
+optimizer = torch.optim.Adam([{'params': listener.parameters()}, {'params': speller.parameters()}],
                              lr=conf['training_parameter']['learning_rate'])
-listener_model_path = conf['meta_variable']['checkpoint_dir']+conf['meta_variable']['experiment_name']+'.listener'
-speller_model_path = conf['meta_variable']['checkpoint_dir']+conf['meta_variable']['experiment_name']+'.speller'
+listener_model_path = conf['meta_variable']['checkpoint_dir'] + conf['meta_variable']['experiment_name'] + '.listener'
+speller_model_path = conf['meta_variable']['checkpoint_dir'] + conf['meta_variable']['experiment_name'] + '.speller'
 
 # save checkpoint with the best ler
 best_ler = 1.0
 global_step = 0
-total_steps = total_epochs * len(X_train) // conf['training_parameter']['batch_size']
 
-train_begin = time.time()
-
-for epoch in range(total_epochs):
+while global_step < total_steps:
 
     # Teacher forcing rate linearly decay
-    tf_rate = tf_rate_upperbound - (tf_rate_upperbound-tf_rate_lowerbound)*(global_step/total_steps)
-
-    epoch_begin = time.time()
+    tf_rate = tf_rate_upperbound - (tf_rate_upperbound - tf_rate_lowerbound) * (global_step / total_steps)
 
     # Training
-    for batch_index,(batch_data,batch_label) in enumerate(train_set):
-        batch_loss, batch_ler = batch_iterator(batch_data, batch_label, listener, speller, optimizer, 
+    for batch_index, (batch_data, batch_label) in enumerate(train_set):
+        batch_loss, batch_ler = batch_iterator(batch_data, batch_label, listener, speller, optimizer,
                                                tf_rate, is_training=True, **conf['model_parameter'])
-
         global_step += 1
 
         if global_step % verbose_step == 0:
-            log_writer.add_scalars('loss',{'train':batch_loss}, global_step)
-            log_writer.add_scalars('cer',{'train':np.array([np.array(batch_ler).mean()])}, global_step)
+            log_writer.add_scalars('loss', {'train': batch_loss}, global_step)
+            log_writer.add_scalars('cer', {'train': np.array([np.array(batch_ler).mean()])}, global_step)
+        if global_step % valid_step == 0:
+            break
 
     # Validation
     dev_loss = []
     dev_ler = []
-    for _,(batch_data,batch_label) in enumerate(valid_set):
-        batch_loss, batch_ler = batch_iterator(batch_data, batch_label, listener, speller, optimizer, 
+    for _, (batch_data, batch_label) in enumerate(valid_set):
+        batch_loss, batch_ler = batch_iterator(batch_data, batch_label, listener, speller, optimizer,
                                                tf_rate, is_training=False, **conf['model_parameter'])
         dev_loss.append(batch_loss)
         dev_ler.extend(batch_ler)
 
-    now_loss, now_cer = np.array([sum(dev_loss)/len(dev_loss)]), np.mean(dev_ler)
-    log_writer.add_scalars('loss',{'dev':now_loss}, global_step)
-    log_writer.add_scalars('cer',{'dev':now_cer}, global_step)
+    log_writer.add_scalars('loss', {'dev': np.array([sum(dev_loss) / len(dev_loss)])}, global_step)
+    log_writer.add_scalars('cer', {'dev': np.array([np.array(dev_ler).mean()])}, global_step)
 
-    current = time.time()
-    epoch_elapsed = (current - epoch_begin) / 60.0
-    train_elapsed = (current - train_begin) / 3600.0
-
-    logger.info("epoch: {}, global step: {:6d}, loss: {:.4f}, cer: {:.4f}, elapsed: {:.2f}m {:.2f}h"
-                .format(epoch, global_step, float(now_loss), float(now_cer), epoch_elapsed, train_elapsed))
-
-    """
     # Generate Attention map
     if conf['model_parameter']['bucketing']:
         feature = listener(Variable(batch_data.float()).squeeze(0).cuda())
@@ -103,45 +92,30 @@ for epoch in range(total_epochs):
     else:
         feature = listener(Variable(batch_data.float()).cuda())
     pred_seq, attention_score = speller(feature)
-    
+
     pred_seq = [char.cpu() for char in pred_seq]
     for t in range(len(attention_score)):
         for h in range(len(attention_score[t])):
             attention_score[t][h] = attention_score[t][h].cpu()
     del feature
 
-    att_map = {i:[] for i in range(conf['training_parameter']['batch_size'])}
+    att_map = {i: [] for i in range(conf['training_parameter']['batch_size'])}
     num_head = len(attention_score[0])
     for i in range(conf['training_parameter']['batch_size']):
         for j in range(num_head):
             att_map[i].append([])
-    for t,head_score in enumerate(attention_score):
-        for h,att_score in enumerate(head_score):
-            for idx,att in enumerate(att_score.data.numpy()):
+    for t, head_score in enumerate(attention_score):
+        for h, att_score in enumerate(head_score):
+            for idx, att in enumerate(att_score.data.numpy()):
                 att_map[idx][h].append(att)
-    for i in range(conf['training_parameter']['batch_size']-1):
+    for i in range(conf['training_parameter']['batch_size'] - 1):
         for j in range(num_head):
-            m = np.repeat(np.expand_dims(np.array(att_map[i][j]),0),3,axis=0)
-            log_writer.add_image('attention_'+str(i)+'_head_'+str(j),
-                                 torch.FloatTensor(m), global_step)
-    """
+            m = np.repeat(np.expand_dims(np.array(att_map[i][j]), 0), 3, axis=0)
+            log_writer.add_image('attention_' + str(i) + '_head_' + str(j),
+                                 torch.FloatTensor(m[:, :, :]), global_step)
+
     # Checkpoint
-    if best_ler >= sum(dev_ler)/len(dev_ler):
-        best_ler = sum(dev_ler)/len(dev_ler)
+    if best_ler >= sum(dev_ler) / len(dev_ler):
+        best_ler = sum(dev_ler) / len(dev_ler)
         torch.save(listener, listener_model_path)
         torch.save(speller, speller_model_path)
-
-# Test
-test_loss = []
-test_ler = []
-for _,(batch_data,batch_label) in enumerate(test_set):
-    batch_loss, batch_ler = batch_iterator(batch_data, batch_label, listener, speller, optimizer,
-                                           tf_rate, is_training=False, **conf['model_parameter'])
-    test_loss.append(batch_loss)
-    test_ler.extend(batch_ler)
-
-now_loss, now_cer = np.array([sum(test_loss)/len(test_loss)]), np.mean(test_ler)
-log_writer.add_scalars('loss',{'test':now_loss}, global_step)
-log_writer.add_scalars('cer',{'test':now_cer}, global_step)
-
-logger.info("test loss: {:.4f}, cer: {:.4f}".format(float(now_loss), float(now_cer)))
--- util/timit_preprocess.py
+++ util/timit_preprocess.py
@@ -2,152 +2,185 @@
 # 			  https://github.com/jameslyons/python_speech_features/issues/53
 import os
 import sys
-import timeit; program_start_time = timeit.default_timer()
-import random; random.seed(int(timeit.default_timer()))
-from six.moves import cPickle 
+import wave
+import timeit;
+
+program_start_time = timeit.default_timer()
+import random;
+
+random.seed(int(timeit.default_timer()))
+from six.moves import cPickle
 import numpy as np
+import scipy.io.wavfile as wav
+
 import python_speech_features as features
+
 # a python package for speech features at https://github.com/jameslyons/python_speech_features
 
 if len(sys.argv) != 3:
-	print('Usage: python3 preprocess.py <timit directory> <output_file>')
+    print('Usage: python3 preprocess.py <timit directory> <output_file>')
 
 ##### SCRIPT META VARIABLES #####
-phn_file_postfix = '.PHN'
-wav_file_postfix = '.WAV'
+phn_file_postfix = '.phn'
+RIFF_wav_postfix = '.WAV'
+
+##### Validation split #####
+# default using 5% of data as validation
+val_split = 0.05
+
 data_type = 'float32'
 
-work_dir = os.getcwd()
-
 paths = sys.argv[1]
-train_path	= np.loadtxt("timit_dataset_list/TRAIN_list.csv", dtype=str)
-valid_path	= np.loadtxt("timit_dataset_list/TEST_developmentset_list.csv", dtype=str)
-test_path	= np.loadtxt("timit_dataset_list/TEST_coreset_list.csv", dtype=str)
-target_path	= os.path.join(paths, sys.argv[2])
-
+train_source_path = os.path.join(paths, 'train')
+test_source_path = os.path.join(paths, 'test')
+target_path = os.path.join(paths, sys.argv[2])
 
 # 61 different phonemes
-phonemes = ["b", "bcl", "d", "dcl", "g", "gcl", "p", "pcl", "t", "tcl", "k", "kcl", "dx", "q", "jh", "ch", "s", "sh", "z", "zh", 
-	"f", "th", "v", "dh", "m", "n", "ng", "em", "en", "eng", "nx", "l", "r", "w", "y", 
-	"hh", "hv", "el", "iy", "ih", "eh", "ey", "ae", "aa", "aw", "ay", "ah", "ao", "oy",
-	"ow", "uh", "uw", "ux", "er", "ax", "ix", "axr", "ax-h", "pau", "epi", "h#"]
+phonemes = ["b", "bcl", "d", "dcl", "g", "gcl", "p", "pcl", "t", "tcl", "k", "kcl", "dx", "q", "jh", "ch", "s", "sh",
+            "z", "zh",
+            "f", "th", "v", "dh", "m", "n", "ng", "em", "en", "eng", "nx", "l", "r", "w", "y",
+            "hh", "hv", "el", "iy", "ih", "eh", "ey", "ae", "aa", "aw", "ay", "ah", "ao", "oy",
+            "ow", "uh", "uw", "ux", "er", "ax", "ix", "axr", "ax-h", "pau", "epi", "h#"]
 
-phonemes2index = {k:v for v,k in enumerate(phonemes)}
+phonemes2index = {k: v for v, k in enumerate(phonemes)}
 
 
 def get_total_duration(file):
-	"""Get the length of the phoneme file, i.e. the 'time stamp' of the last phoneme"""
-	for line in reversed(list(open(file))):
-		[_, val, _] = line.split()
-		return int(val)
+    """Get the length of the phoneme file, i.e. the 'time stamp' of the last phoneme"""
+    for line in reversed(list(open(file))):
+        [_, val, _] = line.split()
+        return int(val)
 
 
 def create_mfcc(filename):
-	"""Perform standard preprocessing, as described by Alex Graves (2012)
+    """Perform standard preprocessing, as described by Alex Graves (2012)
 	http://www.cs.toronto.edu/~graves/preprint.pdf
 	Output consists of 12 MFCC and 1 energy, as well as the first derivative of these.
 	[1 energy, 12 MFCC, 1 diff(energy), 12 diff(MFCC)
 	"""
 
-	rate, sample = 16000, np.fromfile(filename, dtype=np.int16)[512:]
+    (rate, sample) = wav.read(filename)
+    rate, sample = 16000, np.fromfile(filename, dtype=np.int16)[512:]
 
-	mfcc = features.mfcc(sample, rate, winlen=0.025, winstep=0.01, numcep = 13, nfilt=26,
-	preemph=0.97, appendEnergy=True)
-	d_mfcc = features.delta(mfcc, 2)
-	a_mfcc = features.delta(d_mfcc, 2)
+    mfcc = features.mfcc(sample, rate, winlen=0.025, winstep=0.01, numcep=13, nfilt=26,
+                         preemph=0.97, appendEnergy=True)
+    d_mfcc = features.delta(mfcc, 2)
+    a_mfcc = features.delta(d_mfcc, 2)
 
-	out = np.concatenate([mfcc, d_mfcc, a_mfcc], axis=1)
+    out = np.concatenate([mfcc, d_mfcc, a_mfcc], axis=1)
 
-	return out, out.shape[0]
+    return out, out.shape[0]
 
 
 def calc_norm_param(X):
-	"""Assumes X to be a list of arrays (of differing sizes)"""
-	total_len = 0
-	mean_val = np.zeros(X[0].shape[1]) # 39
-	std_val = np.zeros(X[0].shape[1]) # 39
-	for obs in X:
-		obs_len = obs.shape[0]
-		mean_val += np.mean(obs,axis=0)*obs_len
-		std_val += np.var(obs, axis=0)
-		total_len += obs_len
-	
-	mean_val /= total_len
-	std_val **= 0.5
+    """Assumes X to be a list of arrays (of differing sizes)"""
+    total_len = 0
+    mean_val = np.zeros(X[0].shape[1])
+    std_val = np.zeros(X[0].shape[1])
+    for obs in X:
+        obs_len = obs.shape[0]
+        mean_val += np.mean(obs, axis=0) * obs_len
+        std_val += np.std(obs, axis=0) * obs_len
+        total_len += obs_len
 
-	return mean_val, std_val, total_len
+    mean_val /= total_len
+    std_val /= total_len
+
+    return mean_val, std_val, total_len
+
 
 def normalize(X, mean_val, std_val):
-	for i in range(len(X)):
-		X[i] = (X[i] - mean_val)/std_val
-	return X
+    for i in range(len(X)):
+        X[i] = (X[i] - mean_val) / std_val
+    return X
+
 
 def set_type(X, type):
-	for i in range(len(X)):
-		X[i] = X[i].astype(type)
-	return X
+    for i in range(len(X)):
+        X[i] = X[i].astype(type)
+    return X
 
 
-def preprocess_dataset(file_list):
-	"""Preprocess data, ignoring compressed files and files starting with 'SA'"""
-	i = 0
-	X = []
-	Y = []
+def preprocess_dataset(source_path):
+    """Preprocess data, ignoring compressed files and files starting with 'SA'"""
+    i = 0
+    X = []
+    Y = []
 
-	for fname in file_list:
-		phn_fname = "{}/{}{}".format(paths, fname, phn_file_postfix)
-		wav_fname = "{}/{}{}".format(paths, fname, wav_file_postfix)
+    for dirName, subdirList, fileList in os.walk(source_path):
+        for fname in fileList:
+            if not fname.endswith(phn_file_postfix):
+                continue
 
-		total_duration = get_total_duration(phn_fname)
-		fr = open(phn_fname)
+            phn_fname = dirName + '/' + fname
+            wav_fname = dirName + '/' + fname[0:-4] + RIFF_wav_postfix
 
-		X_val, total_frames = create_mfcc(wav_fname)
-		total_frames = int(total_frames)
+            total_duration = get_total_duration(phn_fname)
+            fr = open(phn_fname)
 
-		X.append(X_val)
+            X_val, total_frames = create_mfcc(wav_fname)
+            total_frames = int(total_frames)
 
-		y_val = np.zeros(total_frames) - 1
-		start_ind = 0
-		for line in fr:
-			[start_time, end_time, phoneme] = line.rstrip('\n').split()
-			start_time = int(start_time)
-			end_time = int(end_time)
+            X.append(X_val)
 
-			phoneme_num = phonemes2index[phoneme] if phoneme in phonemes2index else -1
-			end_ind = int(np.round((end_time) / total_duration * total_frames))
-			y_val[start_ind:end_ind] = phoneme_num
+            y_val = np.zeros(total_frames) - 1
+            start_ind = 0
+            for line in fr:
+                [start_time, end_time, phoneme] = line.rstrip('\n').split()
+                start_time = int(start_time)
+                end_time = int(end_time)
 
-			start_ind = end_ind
-		fr.close()
+                phoneme_num = phonemes2index[phoneme] if phoneme in phonemes2index else -1
+                end_ind = int(np.round((end_time) / total_duration * total_frames))
+                y_val[start_ind:end_ind] = phoneme_num
 
-		if -1 in y_val:
-			print('WARNING: -1 detected in TARGET')
-			print(y_val)
+                start_ind = end_ind
+            fr.close()
 
-		Y.append(y_val.astype('int32'))
+            if -1 in y_val:
+                print('WARNING: -1 detected in TARGET')
+                print(y_val)
 
-		i += 1
-		print('file No.', i, end='\r', flush=True)
+            Y.append(y_val.astype('int32'))
 
-	print('Done')
-	return X, Y
+            i += 1
+            print('file No.', i, end='\r', flush=True)
+
+    print('Done')
+    return X, Y
 
 
 ##### PREPROCESSING #####
 print()
 
-print('Preprocessing train data...')
-X_train, y_train = preprocess_dataset(train_path)
-print('Preprocessing valid data...')
-X_valid, y_valid = preprocess_dataset(valid_path)
-print('Preprocessing test data...')
-X_test, y_test = preprocess_dataset(test_path)
+print('Preprocessing training data...')
+X_train_all, y_train_all = preprocess_dataset(train_source_path)
+print('Preprocessing testing data...')
+X_test, y_test = preprocess_dataset(test_source_path)
 print('Preprocessing completed.')
 
 print()
-print('Collected {} training instances (should be 3696 in complete TIMIT )'.format(len(X_train)))
-print('Collected {} validating instances (should be 400 in complete TIMIT )'.format(len(X_valid)))
-print('Collected {} testing instances (should be 192 in complete TIMIT )'.format(len(X_test)))
+train_size = len(X_train_all)
+print(
+    'Collected {} training instances from {} (should be 4620 in complete TIMIT )'.format(train_size, train_source_path))
+print(
+    'Collected {} testing instances from {} (should be 1680 in complete TIMIT )'.format(len(X_test), test_source_path))
+
+print('Spliting {} out of {} ({}%) training data as validation set.'.format(int(train_size * val_split), train_size,
+                                                                            val_split * 100))
+val_idx = [int(i) for i in random.sample(range(0, train_size), int(train_size * val_split))]
+
+X_train = [];
+X_val = []
+y_train = [];
+y_val = []
+for i in range(len(X_train_all)):
+    if i in val_idx:
+        X_val.append(X_train_all[i])
+        y_val.append(y_train_all[i])
+    else:
+        X_train.append(X_train_all[i])
+        y_train.append(y_train_all[i])
 
 print()
 print('Normalizing data to let mean=0, sd=1 for each channel.')
@@ -155,19 +188,19 @@ print('Normalizing data to let mean=0, sd=1 for each channel.')
 mean_val, std_val, _ = calc_norm_param(X_train)
 
 X_train = normalize(X_train, mean_val, std_val)
-X_valid	= normalize(X_valid, mean_val, std_val)
-X_test 	= normalize(X_test, mean_val, std_val)
+X_val = normalize(X_val, mean_val, std_val)
+X_test = normalize(X_test, mean_val, std_val)
 
 X_train = set_type(X_train, data_type)
-X_valid	= set_type(X_valid, data_type)
-X_test 	= set_type(X_test, data_type)
+X_val = set_type(X_val, data_type)
+X_test = set_type(X_test, data_type)
 
 print()
-print('Saving data to ',target_path)
+print('Saving data to ', target_path)
 with open(target_path + '.pkl', 'wb') as cPickle_file:
     cPickle.dump(
-        [X_train, y_train, X_valid, y_valid, X_test, y_test],
-        cPickle_file, 
+        [X_train, y_train, X_val, y_val, X_test, y_test],
+        cPickle_file,
         protocol=cPickle.HIGHEST_PROTOCOL)
 
 print()
