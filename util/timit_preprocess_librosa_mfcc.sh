e81563cd582df606d96e2c528906211b14cc599a
hanseokhyeon <sah0322@naver.com>
2020-04-16 15:27:06 +0900

    make mfcc and spikegram py

---
+++ util/timit_preprocess_librosa_mfcc.sh
@@ -0,0 +1,7 @@
+if [ "$#" -ne 1 ]; then
+    echo "Usage : ./timit_preprocess.sh <timit folder>"
+fi
+echo 'Transfering raw TIMIT wave file format from NIST to RIFF.'
+echo ' '
+# MFCC
+python3 timit_preprocess.py $1 timit_mfcc_39
