bc891d806f4be3c4c2d9947a8cc1068a16069c4b
hanseokhyeon <sah0322@naver.com>
2020-05-15 11:29:43 +0900

    remove slicing for debugging

--- util/copy_phn.py
+++ util/copy_phn.py
@@ -3,7 +3,7 @@ from shutil import copy
 
 list_names = ["timit_dataset_list/TRAIN_list.csv", "timit_dataset_list/TEST_developmentset_list.csv", "timit_dataset_list/TEST_coreset_list.csv"]
 
-for list_name in list_names[:]:
+for list_name in list_names:
     dataset_list = np.loadtxt(list_name, delimiter=',', dtype=np.str)
 
     for filename in dataset_list:
