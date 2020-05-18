270cc29dc948b6d2fdd3c22602d67d32f1706689
hanseokhyeon <sah0322@naver.com>
2020-05-18 11:42:00 +0900

    test print length 4 -> 6

--- train_timit.py
+++ train_timit.py
@@ -110,5 +110,4 @@ model.load_state_dict(torch.load(model_path))
 model.eval()
 
 test_cer = evaluate(test_set, model, tf_rate, conf, global_step, log_writer, epoch_begin, train_begin, logger, epoch, False)
-print(test_cer)
-logger.info("test result, cer: {:.4f}".format(float(test_cer)))
+logger.info("test result, cer: {:.6f}".format(float(test_cer)))
