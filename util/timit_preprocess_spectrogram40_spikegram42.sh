84b3bf6402691a5fcf2e9e58128ed76cdaa63341
hanseokhyeon <sah0322@naver.com>
2020-06-04 13:07:54 +0900

    add spectrogram40 spikegram42

---
+++ util/timit_preprocess_spectrogram40_spikegram42.sh
@@ -0,0 +1,7 @@
+if [ "$#" -ne 1 ]; then
+    echo "Usage : ./timit_preprocess.sh <timit folder>"
+fi
+echo 'Transfering raw TIMIT wave file format from NIST to RIFF.'
+echo ' '
+# MFCC
+python3 timit_preprocess_spectrogram40_spikegram42.py $1 timit_spectrogram_spikegram_246
