5c0fceba538be91908ef5665f7f782bca250f2f0
hanseokhyeon <sah0322@naver.com>
2020-06-10 15:23:26 +0900

    fix directory & add mel80

---
+++ util/timit/timit_preprocess_mel80.sh
@@ -0,0 +1,7 @@
+if [ "$#" -ne 1 ]; then
+    echo "Usage : ./timit_preprocess.sh <timit folder>"
+fi
+echo 'Transfering raw TIMIT wave file format from NIST to RIFF.'
+echo ' '
+# MFCC
+python3 timit_preprocess_mel80.py $1 timit_mel_spectrogram_240
