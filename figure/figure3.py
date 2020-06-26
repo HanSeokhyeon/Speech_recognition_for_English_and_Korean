d0a65e8ff074fc6d03f3ee8076793b2a7a8a5cc6
hanseokhyeon <sah0322@naver.com>
2020-06-26 13:57:25 +0900

    figure 3

--- figure/figure3.py
+++ figure/figure3.py
@@ -13,7 +13,7 @@ fig = plt.figure(figsize=(8, 10))
 
 ###################################################################
 
-plt.subplot(3,1,1)
+plt.subplot(3, 1, 1)
 signal = np.fromfile("SI648.WAV", dtype=np.int16)[512:]
 spectrogram, frequency, time, _ = plt.specgram(x=signal, Fs=16000, NFFT=400, noverlap=160)
 
@@ -26,10 +26,10 @@ plt.ylabel("Frequency [Hz]")
 
 ###################################################################
 
-plt.subplot(3,1,2)
+plt.subplot(3, 1, 2)
 mel_spectrogram = librosa.feature.melspectrogram(y=signal/32767.5, sr=16000, n_fft=400, hop_length=160, n_mels=40)
 display.specshow(librosa.power_to_db(mel_spectrogram, ref=np.max), y_axis='mel', sr=16000, hop_length=160,
-                         x_axis='time')
+                         x_axis='time', fmax=7700)
 
 mel_filter_center_freq = np.argmax(librosa.filters.mel(sr=16000, n_fft=400, n_mels=40).T, axis=0) * 40
 
@@ -43,9 +43,31 @@ plt.ylabel("Band")
 
 ###################################################################
 
-plt.subplot(3,1,3)
+plt.subplot(3, 1, 3)
 
+freq_value = [20, 48, 80, 116, 156, 201, 250, 306, 367, 436, 513, 599, 695, 801, 921, 1053, 1202, 1367, 1551, 1757,
+              1987, 2243, 2528, 2847, 3202, 3599, 4041, 4534, 5085, 5698, 6383, 7147]
 
+x = np.fromfile("SI648_spike.raw", dtype=np.float64)
+x = x.reshape(-1, 4)
 
+num = np.fromfile("SI648_num.raw", dtype=np.int32)
+num_acc = [sum(num[:i+1]) for i in range(len(num))]
+for i, v in enumerate(num_acc[:-1]):
+    x[num_acc[i]:num_acc[i+1], 2] += 12288*(i+1)
+
+x[:, 0] = np.vectorize(lambda a: freq_value[int(a)])(x[:, 0])
+
+plt.scatter(x=x[:, 2], y=x[:, 0], s=x[:, 1]*0.0005, c='black')
+
+plt.title("Spikegram")
+
+plt.xticks([16000, 32000, 48000], [1, 2, 3])
+plt.xlim(0, len(signal))
+plt.xlabel("Time [s]")
+
+plt.yticks([freq_value[i] for i in [10, 20, 30]], [10, 20, 30])
+plt.ylim(0, 8000)
+plt.ylabel("Band")
 
 plt.show()
