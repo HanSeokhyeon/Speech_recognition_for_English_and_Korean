6bc8689ec4849e355b4667a9aacdfec73d814fd6
hanseokhyeon <sah0322@naver.com>
2020-08-10 19:43:31 +0900

    modify figure 9

--- figure/figure9.py
+++ figure/figure9.py
@@ -7,22 +7,23 @@ plt.rcParams['font.family'] = 'Times New Roman'
 plt.rcParams['font.size'] = 20
 fig = plt.figure(figsize=(8, 8), edgecolor='k')
 
+X_mel, _, _, _, _, _ = load_dataset(data_path='dataset/TIMIT/timit_mel_spectrogram_96.pkl')
+mel = np.concatenate(X_mel, axis=0)[:, :32].T
+
+mel_x = np.repeat(range(0, 32), mel.shape[1])
+mel_y = np.reshape(mel, -1)
+
 X_train, _, _, _, _, _ = load_dataset(data_path='dataset/TIMIT/timit_mel_spikegram_240.pkl')
 
 X = np.concatenate(X_train, axis=0)[:, :72]
-mel = X[:, :40].T
 spike = X[:, 40:].T
-
-mel_x = np.repeat(range(0, 40), mel.shape[1])
-mel_y = np.reshape(mel, -1)
-
-
-hist_mel, _, _, _ = plt.hist2d(mel_x, mel_y, bins=(40, 1000), range=[[0, 40], [-3, 3]], cmap='binary', vmax=4000)
-hist_mel[np.where(hist_mel > 4000)] = 4000
-hist_mel = (hist_mel / 4000).T
 
 spike_x = np.repeat(range(0, 32), spike.shape[1])
 spike_y = np.reshape(spike, -1)
+
+hist_mel, _, _, _ = plt.hist2d(mel_x, mel_y, bins=(32, 1000), range=[[0, 32], [-3, 3]], cmap='binary', vmax=4000)
+hist_mel[np.where(hist_mel > 4000)] = 4000
+hist_mel = (hist_mel / 4000).T
 
 hist_spike, _, _, _ = plt.hist2d(spike_x, spike_y, bins=(32, 1000), range=[[0, 32], [-3, 3]], cmap='binary', vmax=4000)
 hist_spike[np.where(hist_spike > 4000)] = 4000
