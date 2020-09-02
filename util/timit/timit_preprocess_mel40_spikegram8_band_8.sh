2b53c00faf1aa75950762c77a409ce263e91d3ab
hanseokhyeon <sah0322@naver.com>
2020-09-02 15:43:04 +0900

    8 band spikegram

---
+++ util/timit/timit_preprocess_mel40_spikegram8_band_8.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+if [ "$#" -ne 1 ]; then
+    echo "Usage : ./timit_preprocess.sh <timit folder>"
+fi
+echo 'Transfering raw TIMIT wave file format from NIST to RIFF.'
+echo ' '
+# MFCC
+python3 timit_preprocess_mel40_spikegram8_band_8.py $1 timit_mel_spikegram_168
